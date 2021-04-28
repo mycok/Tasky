9112af8a36ab1e4bc040347adf1cff6a7c4cdeb4
myko <awesomeme155@gmail.com>
2021-04-28 23:11:43 +0300

    feat: add data model
    - add checklistItem model
    - add checklistItem model ref to the view controller

--- Tasky/ChecklistViewController.swift
+++ Tasky/ChecklistViewController.swift
@@ -8,43 +8,71 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
+    var items = [ChecklistItem]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let item1 = ChecklistItem()
+            item1.text = "Walk the dog"
+            items.append(item1)
+            
+            let item2 = ChecklistItem()
+            item2.text = "Brush my teeth"
+            item2.checked = true
+            items.append(item2)
+            
+            let item3 = ChecklistItem()
+            item3.text = "Learn iOS development"
+            item3.checked = true
+            items.append(item3)
+            
+            let item4 = ChecklistItem()
+            item4.text = "Soccer practice"
+            items.append(item4)
+            
+            let item5 = ChecklistItem()
+            item5.text = "Eat ice cream"
+            items.append(item5)
     }
     
-    func configureCheckmark(for cell: UITableViewCell, at indexPath: IndexPath) {
-        var isChecked = false
-        
-        if indexPath.row == 0 {
-            isChecked = true
-        }
-        
-        if isChecked {
+    func configureCheckmark(for cell: UITableViewCell, with item: ChecklistItem) {
+        if item.checked {
             cell.accessoryType = .checkmark
         } else {
             cell.accessoryType = .none
         }
-        
+    }
+    
+    func configureText(for cell: UITableViewCell, with item: ChecklistItem) {
+        let label = cell.viewWithTag(1000) as! UILabel
+        label.text = item.text
     }
     
 // MARK:- Table View Data source
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return items.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
-        cell.textLabel?.text = "Hello ios"
+        let item = items[indexPath.row]
+        configureText(for: cell, with: item)
+        configureCheckmark(for: cell, with: item)
         
         return cell
     }
     
 //    MARK:- Table View Delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        <#code#>
+        if let cell = tableView.cellForRow(at: indexPath) {
+            let item = items[indexPath.row]
+            item.toggleChecked()
+            configureCheckmark(for: cell, with: item)
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 
